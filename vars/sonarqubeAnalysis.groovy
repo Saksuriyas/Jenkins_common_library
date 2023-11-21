badd0c5b6cac04f372b77396901853962b6b6d75
Saksuriyas <saksuriya.iwaxa@gmail.com>
2023-11-21 23:55:50 +0700

    Update Jenkinfile & sonarqubeAnalysis

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call(String projectName) {
     withSonarQubeEnv('sonar-server') {
-        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=$projectName -Dsonar.projectKey=$projectName '''
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=${projectName} '''
     }
 }
